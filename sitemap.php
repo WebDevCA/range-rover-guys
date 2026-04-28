6169019296c896a2f204e4ac7be577f291893a28
WebDevCA <christianz@netscape.net>
2026-04-27 17:40:22 -0700

    Format sitemap.php with consistent PHP tags

--- sitemap.php
+++ sitemap.php
@@ -16,7 +16,6 @@ include ('header.php');
 						<li><a href="/">Home</a></li>
 						<li><a href="air-conditioning-service-and-repair.php">Air Conditioning Service</a></li>
 						<li><a href="brake-service-and-repair.php">Brake Service</a></li>
-						<li><a href="electrical-diagnosis.php">Electrical Diagnosis</a></li>
 						<li><a href="sitemap.php">Site Map</a></li>
 					</ul>
 
